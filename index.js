0b667830f2da29562b026ceb9b7436ef6da78819
Omar <otantawy53@gmail.com>
2022-04-04 16:34:05 -0400

    commit tree 155e5f5cc18f211244d8fb1d3975546e3cfc58f2

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a , b){
+  return a + b;
+}
+//console.log(add(800 , 90))
+
+function subtract(a , b){
+  return a - b;
+}
+//console.log(subtract(510 , 5));
+
+function multiply(a , b) {
+  return a * b;
+}
+//console.log(multiply(33433 , 2));
+
+function divide(a , b) {
+  return a / b;
+}
+//console.log(divide(0.136986301369863 , 1));
+
+function increment(a) {
+    return ++a;
+  }
+//console.log(increment(161));
+
+function decrement(a){
+  return --a;
+}
+//console.log(decrement(150));
+
+function makeInt(a){
+  return parseInt(a , 10);
+}
+
+function preserveDecimal(a){
+  return parseFloat(a);
+}
